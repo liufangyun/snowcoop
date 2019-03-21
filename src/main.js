b1090467461d8312b1adc624e4ca66ac2e270fff
Lu Xie <xiemegan447@gmail.com>
2019-03-21 12:41:13 -0400

    key added

--- src/main.js
+++ src/main.js
@@ -1,23 +1,30 @@
 // cores and libs
-import Vue from 'vue';
-import App from './app.component.vue';
-import VueRouter from 'vue-router';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import VueForm from 'vue-form';
+import Vue from 'vue'
+import App from './app.component.vue'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueForm from 'vue-form'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 // internal libs
-import router from './router';
-import store from './store';
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.use(VueRouter);
-Vue.use(ElementUI);
-Vue.use(VueForm);
+Vue.use(VueRouter)
+Vue.use(ElementUI)
+Vue.use(VueForm)
 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: '',
+    libraries: 'places'
+  }
+})
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
